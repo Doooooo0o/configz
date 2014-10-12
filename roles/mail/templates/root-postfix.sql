f173cb3be7661cb72ebde5a65bc6c556e93e9fff
theonlydoo <theonlydoo@github.com>
2014-10-12 15:01:10 +0200

    database creation fixed

--- roles/mail/templates/root-postfix.sql
+++ roles/mail/templates/root-postfix.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE postfix;
-GRANT ALL PRIVILEGES ON postfix.* TO 'postfix_admin'@'%' IDENTIFIED BY '{{ dbpassword }}';
-GRANT SELECT ON postfix.* TO 'postfix'@'%' IDENTIFIED BY '{{ dbpassword }}';
+GRANT ALL PRIVILEGES ON postfix.* TO 'postfix_admin'@'%' IDENTIFIED BY '{{ dbpassword.stdout }}';
+GRANT SELECT ON postfix.* TO 'postfix'@'%' IDENTIFIED BY '{{ dbpassword.stdout }}';
 FLUSH PRIVILEGES;
