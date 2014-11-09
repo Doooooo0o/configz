5f39caa398a526f6c037186db87ddb620a05231b
theonlydoo <theonlydoo@github.com>
2014-11-09 12:22:03 +0100

    drop de test, pour valider le playbook de bout en bout

--- roles/mail/templates/root-postfix.sql
+++ roles/mail/templates/root-postfix.sql
@@ -1,4 +1,6 @@
+DROP DATABASE IF EXISTS postfix;
+DROP USER 'postfix'@'%';
 CREATE DATABASE postfix;
-GRANT ALL PRIVILEGES ON postfix.* TO 'postfix_admin'@'%' IDENTIFIED BY '{{ dbpassword.stdout }}';
+GRANT ALL PRIVILEGES ON postfix.* TO 'postfix'@'%' IDENTIFIED BY '{{ dbpassword.stdout }}';
 GRANT SELECT ON postfix.* TO 'postfix'@'%' IDENTIFIED BY '{{ dbpassword.stdout }}';
 FLUSH PRIVILEGES;
