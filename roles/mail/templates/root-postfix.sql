8bfd5e1494e7696d491552bf31a279cfe16a11e0
theonlydoo <theonlydoo@github.com>
2014-11-11 13:17:54 +0100

    managesieve typo, here's what happens when you don't unit test your files ...

--- roles/mail/templates/root-postfix.sql
+++ roles/mail/templates/root-postfix.sql
@@ -1,5 +1,4 @@
 DROP DATABASE IF EXISTS postfix;
-DROP USER 'postfix'@'%';
 CREATE DATABASE postfix;
 GRANT ALL PRIVILEGES ON postfix.* TO 'postfix'@'%' IDENTIFIED BY '{{ dbpassword.stdout }}';
 GRANT SELECT ON postfix.* TO 'postfix'@'%' IDENTIFIED BY '{{ dbpassword.stdout }}';
